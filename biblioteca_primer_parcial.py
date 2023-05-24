9e8e4855d15555af9a65e8e9afb46846648fffca
Lucas Bracuto <lucasdamiann@gmail.com>
2023-05-24 00:46:37 -0300

    ejercicio 2

--- biblioteca_primer_parcial.py
+++ biblioteca_primer_parcial.py
@@ -1,4 +1,5 @@
 import json
+import re
 
 
 def read_file(path_file: str) -> list:
@@ -8,7 +9,7 @@ def read_file(path_file: str) -> list:
     with open(path_file, 'r') as file_object:
         return json.load(file_object)['jugadores']
     
-lista_jugadores_original = read_file("primer_parcial\dt.json")
+lista_jugadores_original = read_file("Repo_Github\pp_lab1_Bracuto_Lucas\dt.json")
 
 def print_data(string_received : str) -> None:
     '''Imprime el string recibido por parametro
@@ -24,9 +25,36 @@ def show_dt_players(players_list : list) -> None:
     Return: None'''
     string_players = ""
     if len(players_list) != 0:
-        for player in players_list:
-            string_players += "Nombre: {0} - {1}\n".format(player["nombre"],player["posicion"])
+        for index in range(len(players_list)):
+            string_format = "{0:2} - Nombre: {1:20}- {2}\n=============================================\n"
+            string_players += string_format.format(index,players_list[index]["nombre"],players_list[index]["posicion"])
+        print_data("===============DREAM TEAM PLAYERS============")
         print_data(string_players)
 
 
-show_dt_players(lista_jugadores_original)
+#show_dt_players(lista_jugadores_original)
+
+###2
+
+def show_player_statistics_by_index(player_list : list) ->None:
+    '''Selecciona un jugador por indice y lo muestra con todas sus caracteristicas
+    param: una lista de jugadores
+    return: none'''
+    string_player = ""
+    new_list = []
+    if len(player_list) != 0:
+        show_dt_players(player_list)
+        index_chosen = input("=Ingrese el indice del jugador a mostrar: ")
+        if re.match("^[0-9]{1,2}$",index_chosen) and int(index_chosen) <= len(player_list) and int(index_chosen) >= 0:
+            new_list.append(player_list[int(index_chosen)])
+            for statistics in player_list[int(index_chosen)]["estadisticas"]:  
+                string_format = "{0:33}| {1}\n=============================================\n"             
+                string_player += string_format.format(statistics.capitalize().replace("_"," "),player_list[int(index_chosen)]["estadisticas"][statistics])
+            
+            print_data("\n=============================================\n{3:2} - {0:28}| {1}\n=============================================\n{2}".format(player_list[int(index_chosen)]["nombre"],player_list[int(index_chosen)]["posicion"],string_player,int(index_chosen)))
+        else:
+            print_data("ERROR: Opcion invalida")
+    else:
+        print_data("ERROR: Lista vacia")
+
+show_player_statistics_by_index(lista_jugadores_original)
